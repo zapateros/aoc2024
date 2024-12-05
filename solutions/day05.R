00bb981298fc543903fe23d16a0a7d8d35d54af0
Paul <plschoenmakers@hotmail.com>
2024-12-05 11:59:21 +0100

    Update day05.R

--- solutions/day05.R
+++ solutions/day05.R
@@ -33,6 +33,10 @@ for(i in 1:length(pagesList)){
 cat("Day 5 star 1:", count)
 
 # Star 2
+# This method is not fundamental. I noticed how the rules are written and used this. For example, if the pages are 1, 2, 3, 4, 5 in this order
+# the the rules would be 1|2, 1|3, 1|4, 1|5, 2|3, 2|4, 2|5, 3|4, 3|5, 4|5. If you count the amount of occurences of the left numbers, the number with the most occurences will be the first number (1, 4 times)
+# and the number that doesn't occur on the left is the last number (5, 0 times). This way you can order by amount of occurences and take the middle number.
+# Again: this method only works because of the way the rules are written. If they are not complete, this method won't work.
 middleNumbers <- sapply(incorrect, function(x){
   pages <- pagesList[[x]]                                             # pages, 81 47 79 63 54 42 87 17 37 59 66 ..
   rule  <- rules[rules$page1 %in% pages & rules$page2 %in% pages,]    # the rows of the rules that are relevant
